5072ff56e6477a5355830bfe0f8d416e93a3a526
Pascal Sturmfels <psturm@umich.edu>
2017-08-09 16:34:35 -0400

    added data labels

---
+++ code/data_scripts/DataSet.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+
+from utils.config import get
+
+class DataSet(object):
+
+    def __init__(self,
+                 images,
+                 labels,
+                 oneHot=True,
+                 numClasses=2,
+                 fake_data=False,
+                 dtype=dtypes.uint8,
+                 reshape=False):
+        """Construct a DataSet.
+        """
+        dtype = dtypes.as_dtype(dtype).base_dtype
+        if dtype not in (dtypes.uint8, dtypes.float32):
+            raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
+                            dtype)
+        else:
+            assert images.shape[0] == labels.shape[0], (
+                'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+            self._num_examples = images.shape[0]
+
+            # Convert shape from [num examples, rows, columns, depth]
+            # to [num examples, rows*columns] (assuming depth == 1)
+            if reshape:
+                images = images.reshape(images.shape[0],
+                                        images.shape[1] * images.shape[2] * images.shape[3])
+            if oneHot:
+                labels = self.convert_to_one_hot(labels, numClasses=numClasses)
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    def convert_to_one_hot(self, labels, numClasses=2):
+        oneHotLabels = np.zeros((labels.shape[0], numClasses))
+        oneHotLabels[np.arange(labels.shape[0]), labels] = 1
+        return oneHotLabels
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size, fake_data=False, shuffle=True):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        # Shuffle for the first epoch
+        if self._epochs_completed == 0 and start == 0 and shuffle:
+            perm0 = np.arange(self._num_examples)
+            np.random.shuffle(perm0)
+            self._images = self.images[perm0]
+            self._labels = self.labels[perm0]
+        # Go to the next epoch
+        if start + batch_size > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Get the rest examples in this epoch
+            rest_num_examples = self._num_examples - start
+            images_rest_part = self._images[start:self._num_examples]
+            labels_rest_part = self._labels[start:self._num_examples]
+            # Shuffle the data
+            if shuffle:
+                perm = np.arange(self._num_examples)
+                np.random.shuffle(perm)
+                self._images = self.images[perm]
+                self._labels = self.labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size - rest_num_examples
+            end = self._index_in_epoch
+            images_new_part = self._images[start:end]
+            labels_new_part = self._labels[start:end]
+            return np.concatenate((images_rest_part, images_new_part), axis=0), np.concatenate((labels_rest_part, labels_new_part), axis=0)
+        else:
+            self._index_in_epoch += batch_size
+            end = self._index_in_epoch
+            return self._images[start:end], self._labels[start:end]
